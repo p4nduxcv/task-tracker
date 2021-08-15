dcda77d5705f2d24b83b63db16123c01e7f0989f
pandula <pandula95@gmail.com>
2021-08-15 11:19:46 +0530

    style: :lipstick: add close icons

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,9 +1,13 @@
 import React from "react";
+import { FaTimes } from "react-icons/fa";
 
 const Task = ({ tasks }) => {
   return (
     <div className="task">
-      <h3 key={tasks.key}>{tasks.text}</h3>
+      <h3 key={tasks.key}>
+        {tasks.text} <FaTimes className="deleteIcons" />
+      </h3>
+      <p>{tasks.day}</p>
     </div>
   );
 };
